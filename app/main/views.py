c5c9edc7817610356179a5f89430fed4b560a35b
StrongPosHao <lantian475587063@qq.com>
2018-06-24 01:06:11 +0800

    '完成登录，注册功能以及数据库模型的bug修复'

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,8 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 
 @main.route('/')
 def index():
-<<<<<<< HEAD
     return render_template('main/index.html')
-=======
-    # return render_template('main/index.html')
-    return render_template('tag/tag-index.html')
->>>>>>> dev1
